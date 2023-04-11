f7a51b7fae694c8326c1cb6d341de217d4d7878a
Jeremy Leung <jml230@pitt.edu>
2023-04-11 16:24:31 -0400

    Rename project to mphat (part2)

---
+++ mphat/__init__.py
@@ -0,0 +1,6 @@
+"""A user-friendly, Python tool for clustering pathways from molecular dynamics simulations"""
+
+# Add imports here
+from .mphat import *
+
+from mphat._version import __version__
---
+++ mphat/tests/test_mphat.py
@@ -0,0 +1,15 @@
+"""
+Unit and regression test for the mphat package.
+"""
+
+# Import package, test suite, and other packages as needed
+import sys
+
+import pytest
+
+import mphat
+
+
+def test_mphat_imported():
+    """Sample test, will always pass so long as import statement worked."""
+    assert "mphat" in sys.modules
