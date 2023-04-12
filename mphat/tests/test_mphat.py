d695c8e2dc74df2849c873a90c68627704c2e9ff
Jeremy Leung <jml230@pitt.edu>
2023-04-11 20:53:32 -0400

    Update mPHAT test

--- mphat/tests/test_mphat.py
+++ mphat/tests/test_mphat.py
@@ -4,10 +4,8 @@ Unit and regression test for the mPHAT package.
 
 # Import package, test suite, and other packages as needed
 import sys
-
 import pytest
-
-import mphat
+#import mphat
 
 def test_mphat_imported():
     """Sample test, will always pass so long as import statement worked."""
