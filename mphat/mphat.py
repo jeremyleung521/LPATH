165d55df2632576687cdba378d404d2c5b663ee2
Jeremy Leung <jml230@pitt.edu>
2023-04-13 13:13:52 -0400

    docstrings

--- mphat/mphat.py
+++ mphat/mphat.py
@@ -9,6 +9,14 @@ log = logging.getLogger(__name__)
 
 
 def main(arguments):
+    """
+    Main function to run it all!
+
+    Parameters
+    ----------
+    arguments : argparse.Namespace
+        A namespace with all the parameter arguments
+    """
     discretize.main(arguments)
     print(arguments.assign_name)
     extract.main(arguments)
@@ -44,20 +52,20 @@ if __name__ == "__main__":
         assign_name="ANALYSIS/TEST/assign.h5",  # Name of output assign.h5 file
         rcfile="west.cfg",  # west.cfg file
         assign_args=argparse.Namespace(  # These are arguments for w_assign
-            verbosity='verbose',  # Verbose or debug
+            verbosity="verbose",  # Verbose or debug
             rcfile="west.cfg",  # west.cfg
             max_queue_length=None,
-            we_h5filename='west.h5',  # west.h5 path
+            we_h5filename="west.h5",  # west.h5 path
             construct_dataset=None,  # If you need some custom auxiliary dataset
             dsspecs=None,
-            output='assign.h5',  # Output file
+            output="assign.h5",  # Output file
             subsample=None,
             config_from_file=True,  # Read config from rcfile
-            scheme='TEST',  # Scheme name
+            scheme="TEST",  # Scheme name
         ),
 
         # Extract Parameters
-        # Note west_name and assign_name are repeated from above
+        # Note west_name and assign_name are repeated from above and removed
         source_state_num=0,  # Index of the source state as defined in assign.h5.
         target_state_num=1,  # Index of the target state as defined in assign.h5.
         first_iter=1,  # First iteration to analyze. Inclusive
@@ -71,18 +79,18 @@ if __name__ == "__main__":
         hdf5=False,  # Enable if trajectories are saved with the HDF5 Framework in WESTPA.
         rewrite_weights=False,  # Option to zero out the weights of all segments that are not a successful trajectory.
         pcoord=True,  # Option to output the pcoord into the `output.pickle`.
-        auxdata=['phi', 'psi'],  # Additional auxiliary data to save into `output.pickle`.
+        auxdata=["phi", "psi"],  # Additional auxiliary data to save into `output.pickle`.
         use_ray=False,  # Enable Ray.
         threads=0,  # How many Ray threads/actors to use. Defaults to 0, which wil use all auto-detected resources.
 
         # Match Parameters
-        # Note west_name, assign_name and out_dir are repeated and removed
-        input_pickle='succ_traj/output.pickle',  # Input file name of the pickle from `extract.py`
+        # Note west_name, assign_name and out_dir are repeated from above and removed
+        input_pickle="succ_traj/output.pickle",  # Input file name of the pickle from `extract.py`
         dmatrix_remake=True,  # Enable to remake the distance Matrix
-        dmatrix_save='distmap.npy',  # If dmatrix_remake is False, load this file instead. Assumed located in {out_dir}.
+        dmatrix_save="distmap.npy",  # If dmatrix_remake is False, load this file instead. Assumed located in {out_dir}.
         dendrogram_threshold=0.5,  # Threshold for the Dendrogram
         dendrogram_show=True,  # Show the Dendrogram using plt.show()
-        cl_output='succ_traj/cluster_labels.npy',  # Output path for cluster labels
+        cl_output="succ_traj/cluster_labels.npy",  # Output path for cluster labels
         file_pattern="west_succ_c{}.h5",  # Pattern to name cluster files
         clusters=None,  # Cluster index to output... otherwise None --> All
 
