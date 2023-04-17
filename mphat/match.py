ca23cb8676d8e38ed0be1db927e5e9f8cfb51f07
Jeremy Leung <jml230@pitt.edu>
2023-04-17 18:23:56 -0400

    Update expand_shorter_traj

--- mphat/match.py
+++ mphat/match.py
@@ -260,7 +260,7 @@ def expand_shorter_traj(pathways, dictionary):
     for pathway in pathways:
         for step in pathway:
             if step[0] == 0:  # If no iteration number (i.e., a dummy frame)
-                step[2] = dictionary[-1]  # Mark with the last entry
+                step[2] = len(dictionary)  # Mark with the last entry
 
 
 def gen_dist_matrix(pathways, dictionary, file_name="distmap.npy", out_dir="succ_traj", remake=False):
