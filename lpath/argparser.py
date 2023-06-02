f64de56ee77645f5814440328bc83be5514a50d9
Jeremy Leung <jml230@pitt.edu>
2023-06-02 17:40:17 -0400

    typo in argparser

--- lpath/argparser.py
+++ lpath/argparser.py
@@ -324,7 +324,7 @@ def add_match_args(parser=None):
                           help='Output file location for cluster labels.')
     match_io.add_argument('--exclude-min-length', '-el', '--exclude-length', '--exclude-short', dest='exclude_short',
                           type=check_non_neg, default=0, help='Exclude trajectories shorter than provided value during \
-                          matching. Default is 0, which will include trajectories of all length.')
+                          matching. Default is 0, which will include trajectories of all lengths.')
     match_io.add_argument('--reassign', '-ra', '--reassign-method', dest='reassign_method',
                           default='reassign_identity', type=str,
                           help='Reassign Method to use. Could be one of the defaults or a module to load. Defaults are \
