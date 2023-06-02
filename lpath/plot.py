70b451829322f36757d1648f1d591d908b9e97b0
Jeremy Leung <jml230@pitt.edu>
2023-06-02 15:26:04 -0400

    partial plot.py update

--- lpath/plot.py
+++ lpath/plot.py
@@ -6,15 +6,32 @@ import numpy
 import lpath
 import matplotlib
 import matplotlib.pyplot as plt
+from lpath.match import gen_dist_matrix, hcluster
 from lpath.io import load_file
+from os.path import exists
+
 try:
     from importlib.resources import files
 except ImportError:
     from importlib_resources import files
+
 log = logging.getLogger(__name__)
 
 
-def plot(input_array):
+def plt_config(args):
+    if exists(args.mpl_styles):
+        plt.style.use(args.mpl_styles)
+    else:
+        style_f = (files(lpath) / 'data/styles/default.mplstyle')
+        plt.style.use(style_f)
+        log.debug(f'DEBUG: Using default {style_f}')
+
+    fig, ax = plt.subplots()
+
+    return fig, ax
+
+
+def plot(data, arguments):
     """
     This is an example function for plotting things.
 
@@ -26,8 +43,32 @@ def plot(input_array):
     Returns
     -------
     """
-    inp_file = (files(lpath) / 'data/styles/default.mplstyle')
-    print(inp_file)
+    fig, ax = plt_config(arguments)
+
+    return fig, ax
+
+
+def organize_data(arguments):
+    cluster_labels = load_file(arguments.cl_output, 1)
+    load_data = load_file(arguments.output_pickle, 1)
+
+    data = dict()
+
+    npathways = len(load_data)
+    log.info(f'There are {npathways} pathways.')
+    path_idxs = numpy.arange(0, npathways)
+
+    weights, durations, pathways = [], [], []
+    for pathway in load_data:
+        weights.append(pathway[0][-1])
+        durations.append(int(len(pathway[:])))
+        pathways.append(pathway)
+
+    data['weights'] = numpy.asarray(weights)
+    data['durations'] = numpy.asarray(durations)
+    data['pathways'] = numpy.asarray(pathways)
+
+    return data
 
 
 def main(arguments):
@@ -40,7 +81,9 @@ def main(arguments):
         A Namespace object will all the necessary parameters.
 
     """
-    cluster_labels = load_file(arguments.cl_output, 1)
+
+    data = organize_data(arguments)
+
+    plot(data, arguments)
+
     pass
-
-
