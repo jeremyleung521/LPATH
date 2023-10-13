cb25b09f62f39f8728b108aef7ace74e232bc6fa
Jeremy Leung <jml230@pitt.edu>
2023-10-13 15:29:42 -0400

    plot_network.py update/documentation

--- examples/cMD/plots/plot_network.py
+++ examples/cMD/plots/plot_network.py
@@ -1,12 +1,15 @@
+import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 import pickle
 import scipy.cluster.hierarchy as sch
+import networkx as nx
 from scipy.spatial.distance import squareform
 from itertools import groupby
-import networkx as nx
 from sys import argv
 
+# Default dendrogram colors from LPATH
+default_dendrogram_colors = ['tomato', 'dodgerblue', 'orchid', 'mediumseagreen', 'darkorange', 'mediumpurple', 'grey']
 
 cluster_centers = np.load("../centroids.npy")
 
@@ -48,7 +51,7 @@ for cidx, cluster in enumerate(range(n_clusters)):
 
     plt.title("class "+str(cluster+1))
 
-    path_idxs_c = path_idxs[labels==cluster]
+    path_idxs_c = path_idxs[labels == cluster]
 
     cluster_counts, state_list, edge_list = [], [], []
 
@@ -64,15 +67,21 @@ for cidx, cluster in enumerate(range(n_clusters)):
 
             weighted_counts = np.array([np.sum(weights_last[states_last == i]) for i in range(12)], dtype=float)
             cluster_counts.append(weighted_counts)
-            condensed_state = [int(k) for k,g in groupby(states)]
+            condensed_state = [int(k) for k, g in groupby(states)]
+
+            # Adding 6 and 7 as our source and target states
             condensed_state.insert(0, 6)
             condensed_state.insert(len(condensed_state), 7)
+
+            # In this section, we're creating "virtual"/duplicate/invisible states off the network map
+            # to account for state-to-state transitions over the periodic boundaries in
+            # alanine dipeptide's phi/psi plot.
+            # The clusters in "pstates" are mapped to the virtual states in "istates"
             pstates = [2, 3, 1, 5]
             istates = [8, 9, 10, 11]
 
             if condensed_state not in state_list:
                 state_list.append(condensed_state)
-
                 for c in range(len(condensed_state)-1):
                     edge = [condensed_state[c], condensed_state[c+1]]
                     if edge == [pstates[1],pstates[0]]:
@@ -90,6 +99,7 @@ for cidx, cluster in enumerate(range(n_clusters)):
                     else:
                         edge_list.append(edge)
 
+    # Manually resizing each state's size by a custom amount.
     sum_counts = np.sum(cluster_counts, axis=0)
     sum_counts = sum_counts*10000
     sum_counts[6] = 500
@@ -99,14 +109,18 @@ for cidx, cluster in enumerate(range(n_clusters)):
     sum_counts[10] = 1
     sum_counts[11] = 1
 
+    n_total_states = len(sum_counts)  # Automatically grabbing how many states we have.
+
+    # Drawing the directed graph.
     G = nx.DiGraph()
-    G.add_nodes_from([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]) # 8 and 9 are start and end, 10-13 are periodic
+    G.add_nodes_from(list(range(n_total_states)))  # 8 and 9 are start and end, 10-13 are periodic
     G.add_edges_from(edge_list)
+
+    # Mapping state id to the state label to be shown on this network plot
     mapping = {0: "0", 1: "1", 2: "2", 3: "3", 4: "4", 5: "5", 6: "A", 7: "B", 8: "8", 9: "9", 10: "10", 11: "11"}
-    if cidx == 0:
-        colors = ["#FF6347", "#FF6347", "#FF6347", "#FF6347", "#FF6347", "#FF6347", "#D3D3D3", "#D3D3D3", "#FF6347", "#FF6347", "#FF6347", "#FF6347"]
-    else:
-        colors = ["#1E90FF", "#1E90FF", "#1E90FF", "#1E90FF", "#1E90FF", "#1E90FF", "#D3D3D3", "#D3D3D3", "#1E90FF", "#1E90FF", "#1E90FF", "#1E90FF"]
+    colors = [matplotlib.colors.cnames[default_dendrogram_colors[cidx]] if mapping[i] not in ["A", "B"] else "#D3D3D3" for i in range(n_total_states)]
+
+    # Specifying custom positions of each state
     pos = {"0": cluster_centers[0], "1": cluster_centers[1], "2": cluster_centers[2], "3": cluster_centers[3], "4": cluster_centers[4], "5": cluster_centers[5], "A": (-140,80), "B": (80,-50), "8": (-110, 250), "9": (-110, -250), "10": (60, 250), "11": (60, -250)}
     isolated = list(nx.isolates(G))
     new_isolated = []
@@ -115,6 +129,8 @@ for cidx, cluster in enumerate(range(n_clusters)):
         colors = np.delete(colors, isolated)
     G.remove_nodes_from(list(nx.isolates(G)))
     G = nx.relabel_nodes(G, mapping)
+
+    # Finally drawing the networkx plots.
     nx.draw_networkx_nodes(G, pos=pos, node_color=colors, alpha=0.5, node_size=sum_counts)
     nx.draw_networkx_edges(G, pos=pos, arrows=True, node_size=250, width=1.5)
     nx.draw_networkx_labels(G, pos=pos, font_weight='bold')
