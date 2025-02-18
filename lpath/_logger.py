c0aeecb0184ee3a75d19a2248ebe374aa604cc1c
Jeremy Leung <jml230@pitt.edu>
2025-02-18 14:34:28 -0500

    remove the pesky SyntaxWarning with raw strings

--- lpath/_logger.py
+++ lpath/_logger.py
@@ -46,7 +46,7 @@ class ColorizedArgsFormatter(logging.Formatter):
             color = ColorizedArgsFormatter.level_to_color[level]
             _format = fmt
             for fld in ColorizedArgsFormatter.level_fields:
-                search = "(%\(" + fld + "\).*?s)"
+                search = r"(%\(" + fld + r"\).*?s)"
                 _format = re.sub(search, f"{color}\\1{ColorCodes.reset}", _format)
             formatter = logging.Formatter(_format)
             self.level_to_formatter[level] = formatter
